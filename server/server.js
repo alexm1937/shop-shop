04fb03505e8f04c91c84d7b97ca8cb79764cfda5
unknown <alexm1397@gmail.com>
2022-01-29 16:35:44 -0600

    added some comments and slight refactor

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+// start Apollo server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -22,10 +22,11 @@ app.use(express.json());
 // Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
+//check if production env 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
+//catch any other req's send to homepage
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
